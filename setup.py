aa3b5d61d45cef9b935a767f4cb278a7a6bec1e3
Daniel Sabinasz <daniel.sabinasz@ruhr-uni-bochum.de>
2022-08-10 14:11:41 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='DynamicFieldPy',
-      version='0.1',
+      version='0.1.1',
       description='A library for creating Dynamic Field architectures with Python',
       url='https://github.com/danielsabinasz/DynamicFieldPy',
       author='Daniel Sabinasz',
